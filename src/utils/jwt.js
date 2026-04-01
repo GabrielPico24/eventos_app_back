63e84bf96d81f05c1cc499a7aea8f5b78a1e11ef
josekage <jose-luis01@hotmail.es>
2026-04-01 17:20:18 -0500

    prueba render2

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -3,7 +3,7 @@ const { JWT_SECRET } = require('../config/env');
 
 const generateAccessToken = (payload) => {
   return jwt.sign(payload, JWT_SECRET, {
-    expiresIn: '5m',
+    expiresIn: '15m',
   });
 };
 
