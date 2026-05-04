0b59daef294d3f4e6466bb2004d068872bb56179
gabrielpico26 <kevin.pico12@hotmail.com>
2026-05-03 23:56:08 -0500

    presentacion jefe1

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -3,7 +3,7 @@ const { JWT_SECRET } = require('../config/env');
 
 const generateAccessToken = (payload) => {
   return jwt.sign(payload, JWT_SECRET, {
-    expiresIn: '1h',
+    expiresIn: '15m',
   });
 };
 
